eb944dfeb36979075704c6216f5add2c9f77ba67
killla <killa.granet@gmail.com>
2020-02-05 20:34:45 +0500

    Update scripts.py
    Устранены замечания 23-28

--- scripts.py
+++ scripts.py
@@ -2,7 +2,7 @@ import random
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import Schoolkid, Mark, Chastisement, Commendation, Lesson, Subject
 
-commendation_list = [x for x in open('commendations.txt')]
+
 
 def find_child(name):
     try:
@@ -15,11 +15,11 @@ def find_child(name):
 
 def fix_marks(name):
     child = find_child(name)
-    for current_mark in Mark.objects.filter(schoolkid=child, points__lte=3):
-        current_mark.points = 4
-        current_mark.save()
-
-    print('Оценки исправлены.')
+    if child:
+        for current_mark in Mark.objects.filter(schoolkid=child, points__lte=3):
+            current_mark.points = 4
+            current_mark.save()
+        print('Оценки исправлены.')
 
 def remove_chastisements(name):
     child = find_child(name)
@@ -29,12 +29,17 @@ def remove_chastisements(name):
         print('Замечания устранены.')
 
 def create_commendation(name, subject):
+
+    with open('commendations.txt') as commendation_file:
+        commendations = commendation_file.readlines()
+
     child = find_child(name)
-    if child and Subject.objects.filter(title=subject, year_of_study=child.year_of_study):
-        current_lesson = \
-        Lesson.objects.filter(year_of_study=child.year_of_study, group_letter=child.group_letter, subject__title=subject).order_by('-date')[0]
-        Commendation.objects.create(text=random.choice(commendation_list), created=current_lesson.date, schoolkid=child,
-                                    subject=current_lesson.subject, teacher=current_lesson.teacher)
-        print('Ученик похвален.')
-    else:
-        print('Предмет не найден')
+    if child:
+        if Subject.objects.filter(title=subject, year_of_study=child.year_of_study):
+            current_lesson = \
+            Lesson.objects.filter(year_of_study=child.year_of_study, group_letter=child.group_letter, subject__title=subject).order_by('-date')[0]
+            Commendation.objects.create(text=random.choice(commendations), created=current_lesson.date, schoolkid=child,
+                                        subject=current_lesson.subject, teacher=current_lesson.teacher)
+            print('Ученик похвален.')
+        else:
+            print('Предмет не найден')
